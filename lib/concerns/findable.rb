308fd97504c42c82150b985757d4a6e9273aa9b5
Joshua Needham <joshuabneedham@gmail.com>
2017-11-29 19:02:54 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,9 @@
-module Findable
-  module ClassMethods
+module Concerns::Findable
+  def find_by_name(name)
+    self.all.detect { |a| a.name == name }
+  end
 
-    def find_by_name(name)
-      self.all.detect { |a| a.name == name }
-    end
-
+  def find_or_create_by_name
+    find_by_name(name) || create(name)
   end
 end
