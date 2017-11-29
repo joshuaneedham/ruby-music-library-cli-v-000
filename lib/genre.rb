a178b95c38f6fe8b873cecb9aed7e6a82e24c069
Joshua Needham <joshuabneedham@gmail.com>
2017-11-29 18:22:33 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,22 +1,3 @@
 class Genre
-  attr_accessor :name
 
-  def initialize
-    @name = name
-    @songs = []
-  end
-
-  def songs
-    @songs
-  end
-
-  def add_song(song)
-    @songs << song
-  end
-
-  def artist
-    @songs.collect do |song|
-      song.artist
-  end
-end
 end
